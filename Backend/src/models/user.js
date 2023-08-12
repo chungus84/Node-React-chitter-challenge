629509b75cd55a199f7477a631ddf19347804e7c
kenichi beveridge <kenbev84@gmail.com>
2023-08-12 16:20:37 +0100

    Created user class and test

---
+++ Backend/src/models/user.js
@@ -0,0 +1,18 @@
+class User {
+    #fName;
+    #lName;
+    #email;
+    #userName;
+    #password;
+
+    constructor(fName, lName, email, userName, password) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+
+    }
+}
+
+export default User;
---
+++ Backend/tests/user.test.js
@@ -0,0 +1,16 @@
+import chai, { expect } from 'chai';
+import User from '../src/models/user.js';
+
+describe('User Tests', () => {
+    it('should create a new user', () => {
+        const fName = "Joe";
+        const lName = "Blogs";
+        const email = "test@test.com";
+        const userName = "jblog88";
+        const password = "password";
+
+        const testUser = new User(fName, lName, email, userName, password);
+
+        expect(testUser).instanceOf(User);
+    })
+})
